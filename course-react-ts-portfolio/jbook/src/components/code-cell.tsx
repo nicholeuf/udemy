6bdf3ea59a185549494f455a456c850c75b15cb4
Nichole Frey <nicholeuf@users.noreply.github.com>
2023-12-17 09:28:03 -0500

    Automatically bundle code after user stops typing for 1 s

--- course-react-ts-portfolio/jbook/src/components/code-cell.tsx
+++ course-react-ts-portfolio/jbook/src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -9,10 +9,17 @@ const CodeCell = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  // Bundle code after user stops typing 1s
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction='vertical'>
