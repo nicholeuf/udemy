25a2a57958afdf4941385d2f1e2e43925199c35d
Nichole Frey <nicholeuf@users.noreply.github.com>
2023-12-18 11:25:14 -0500

    Rough in redux store implementation

---
+++ course-react-ts-portfolio/jbook/src/state/reducers/cellsReducer.ts
@@ -0,0 +1,75 @@
+import { produce } from 'immer';
+
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+import { Cell } from '../cell';
+
+interface CellsState {
+  loading: boolean;
+  error: string | null;
+  order: string[];
+  data: {
+    [key: string]: Cell;
+  };
+}
+
+const initialState: CellsState = {
+  loading: false,
+  error: null,
+  order: [],
+  data: {},
+};
+
+const reducer = produce(
+  (draft: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL: {
+        const { id, content } = action.payload;
+        draft.data[id].content = content;
+        return draft;
+      }
+      case ActionType.DELETE_CELL: {
+        delete draft.data[action.payload];
+        draft.order = draft.order.filter((id) => id !== action.payload);
+        return draft;
+      }
+      case ActionType.MOVE_CELL: {
+        const { direction } = action.payload;
+        const index = draft.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+        if (targetIndex < 0 || targetIndex > draft.order.length - 1) {
+          return draft;
+        }
+
+        draft.order[index] = draft.order[targetIndex];
+        draft.order[targetIndex] = action.payload.id;
+        return draft;
+      }
+      case ActionType.INSERT_CELL_BEFORE: {
+        const cell: Cell = {
+          content: '',
+          type: action.payload.type,
+          id: randomId(),
+        };
+
+        draft.data[cell.id] = cell;
+        const index = draft.order.findIndex((id) => id === action.payload.id);
+        if (index < 0) {
+          draft.order.push(cell.id);
+        } else {
+          draft.order.splice(index, 0, cell.id);
+        }
+        return draft;
+      }
+      default:
+        return draft;
+    }
+  }
+);
+
+const randomId = () => {
+  return Math.random().toString(36).substring(2, 5);
+};
+
+export default reducer;
